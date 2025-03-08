8db226e6a9ff223af767859eddbff8fa78564e66
Tasmir Hossain Zihad <zihad2107071@stud.kuet.ac.bd>
2025-03-09 03:27:53 +0600

    Implement MFA module with controllers and DTOs for enabling, verifying, and managing multi-factor authentication

---
+++ services/server/src/mfa/dto/s-enable.dto.ts
@@ -0,0 +1,12 @@
+import { IMfaEnableResult } from '../../common/interfaces/mfa.interface'
+import { ApiProperty } from '@nestjs/swagger'
+
+export class MfaEnableResultDto implements IMfaEnableResult {
+  @ApiProperty({
+    description: 'Recovery code for MFA authentication',
+    example: 'AB12CD34EF',
+    required: true,
+    type: String
+  })
+  recoveryCodes: string[]
+}
